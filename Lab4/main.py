625c56e947f5713361bda4d50e5d4f0e4c67db72
st0rmbtw <>
2025-10-21 17:17:58 +0300

    Lab4

---
+++ Lab4/main.py
@@ -0,0 +1,36 @@
+from collections import deque
+type Node = list[int, Node, Node] | None
+
+def gen_bin_tree(
+    height: int,
+    root: int,
+    left_leaf = lambda x: x * 4,
+    right_leaf = lambda x: x + 1
+) -> Node:
+    root_node: Node = [root, None, None]
+    queue = deque([root_node])
+    level = 0
+    while queue:
+        if len(queue) == 2 ** level:
+            level += 1
+        if level == height:
+            break
+        
+        node = queue.popleft()
+        
+        node[1] = [left_leaf(node[0]), None, None]
+        queue.append(node[1])
+        
+        node[2] = [right_leaf(node[0]), None, None]
+        queue.append(node[2])
+    return root_node
+    
+def print_bin_tree(node: Node, indent: int = 0) -> None:
+    if node == None: return
+    value = node[0]
+    print_bin_tree(node[1], indent + 3 + len(str(value)))
+    print(' ' * indent + '-- ' + str(value))
+    print_bin_tree(node[2], indent + 3 + len(str(value)))
+
+if __name__ == "__main__":
+    print_bin_tree(gen_bin_tree(4, 4))
---
+++ Lab4/test.py
@@ -0,0 +1,32 @@
+from main import gen_bin_tree
+import unittest
+
+left_leaf = lambda x: x * 4
+right_leaf = lambda x: x + 1
+
+class Test(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(
+            gen_bin_tree(3, 2), 
+            [2,
+                [left_leaf(2),
+                    [left_leaf(left_leaf(2)),
+                        None,
+                        None],
+                    [right_leaf(left_leaf(2)),
+                        None,
+                        None]],
+                [right_leaf(2),
+                    [left_leaf(right_leaf(2)),
+                        None,
+                        None],
+                    [right_leaf(right_leaf(2)),
+                        None,
+                        None]]]
+        )
+
+    def test2(self):
+        self.assertEqual(gen_bin_tree(1, 0), [0, None, None])
+
+if __name__ == "__main__":
+    unittest.main()
