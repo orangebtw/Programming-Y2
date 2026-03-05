bc6bec5bf1c81549aac4af461beb0d0d8a96a9b6
st0rmbtw <>
2026-03-05 16:05:07 +0300

    Some changes

--- Sem2-Lab3/models/subscription.py
+++ Sem2-Lab3/models/subscription.py
@@ -1,10 +1,8 @@
-from sqlalchemy.orm import MappedColumn, mapped_column
 from models.base import Base
 from sqlalchemy import Column, ForeignKey, Integer
 
 class Subscription(Base):
     __tablename__ = "subscriptions"
-    id: Column[int] = Column(Integer, primary_key=True, autoincrement=True)
-    user_id: Column[int] = Column(Integer, ForeignKey("users.id"))
-    currency_id: Column[int] = Column(Integer, ForeignKey("currencies.id"))
+    user_id: Column[int] = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    currency_id: Column[int] = Column(Integer, ForeignKey("currencies.id"), primary_key=True)
 
--- Sem2-Lab3/routers/subscriptions.py
+++ Sem2-Lab3/routers/subscriptions.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
 from models.subscription import Subscription
 from schemas.subscription import SubscriptionSchema
 from schemas.response import ResponseSchema, ErrorSchema
@@ -11,8 +12,11 @@ router = APIRouter()
 @router.post("/")
 async def subscribe_user_to_currency(schema: SubscriptionSchema, session: AsyncSession = Depends(get_db_session)):
     session.add(Subscription(user_id=schema.user_id, currency_id=schema.currency_id))
-    await session.commit()
-    return ResponseSchema(success=True, error=ErrorSchema(message="User subscribed successfully"))
+    try:
+        await session.commit()
+        return ResponseSchema(success=True, error=ErrorSchema(message="User subscribed successfully"))
+    except IntegrityError:
+        return ResponseSchema(success=False, error=ErrorSchema(message="This user has already subscribed to this currency"))
 
 @router.delete("/")
 async def unsubscribe_user_from_currency(schema: SubscriptionSchema, session: AsyncSession = Depends(get_db_session)):
